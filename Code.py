b884591f7947717fad764e8f0d21710b23b8be04
Ben Danon <bendanon305@gmail.com>
2015-04-13 19:59:19 +0300

    Started code class

--- Code.py
+++ Code.py
@@ -2,3 +2,21 @@
 Translates each field into its corresponding binary value,
 and assembles the resulting values
 '''
+
+
+class Code:
+
+    def __init__(self):
+        self.jump_dict = {}
+        self.comp_dict = {}
+        self.dest_dict = {}
+
+    def dest(self, mnemonic):
+        return self.dest_dict.get(mnemonic)
+
+    def comp(self, mnemonic):
+        return self.comp_dict(mnemonic)
+
+    def jump(self, mnemonic):
+        return self.jump_dict(mnemonic)
+
