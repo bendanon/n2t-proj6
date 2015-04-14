fbff88e60e26f2080eaf079e5d2d9bf978f0b4bf
Ben Danon <bendanon305@gmail.com>
2015-04-14 22:43:48 +0300

    Added some badass regexes

--- Parser.py
+++ Parser.py
@@ -1,20 +1,28 @@
 '''
 Unpacks each command into its underlying fields
 '''
-
-from enum import Enum
-CommandType = Enum('A_COMMAND', 'C_COMMAND', 'L_COMMAND')
-
 import re
 
-
+class CommandType:
+    A = 1
+    C = 2
+    L = 3
 
 class Parser:
+   
+    dest="(?:M=|D=|MD=|A=|AM=|AD=|AMD=)?"
+    jump="(?:;JGT|;JEQ|;JGE|;JLT|;JNE|;JLE|;JMP)?"
+    comp="(?:[AMD][+-]1|[AM]-D|D[&+-][AM]|D\\|[AM]|[01]|[-!]?[AMD])"
+ 
+    re_A_COMMAND = re.compile("@[A-Za-z]*|@[0-9]*")
+    re_C_COMMAND = re.compile(dest + comp + jump)
+    re_L_COMMAND = re.compile("([A-Za-z]*)")
+
+    re_comment = re.compile('//|/*')
     
-    re_A_COMMAND = re.compile('@[A-Z]')
     
     def __init__(self, filename):
-        self.infile = open('workfile', 'r')        
+        self.infile = open(filename, 'r')        
         self.moreCommands = False
         self.currentCommand = None
 
@@ -30,7 +38,28 @@ class Parser:
     Initially, there is no current command
     '''
     def advance(self)
-        return None
+        self.currentCommand = self.infile.readline()
+        cleanCommand()
+
+        while len(self.currentCommand) == 0:
+            self.currentCommand = self.infile.readline()
+            if not self.currentCommand: break
+            cleanCommand()
+
+    '''
+    Removes whitespace and comments
+    '''
+    def cleanCommand(self):
+        self.currentCommand = re_comment.split(self.currentCommand.strip())[0]
 
     def commandType(self):
-        if 
+
+        if re_A_COMMAND.findall(currentCommand)[0] == currentCommand:
+            return CommandType.A
+        elif re_C_COMMAND.findall(currentCommand)[0] == currentCommand:
+            return CommandType.C
+        elif re_L_COMMAND.findall(currentCommand)[0] == currentCommand:
+            return CommandType.L
+
+
+
