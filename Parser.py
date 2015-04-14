31c02a35992c0e175ec7387c3b1476cd4c93a31b
Ben Danon <bendanon305@gmail.com>
2015-04-14 07:16:13 +0300

    Started Parser

--- Parser.py
+++ Parser.py
@@ -2,12 +2,35 @@
 Unpacks each command into its underlying fields
 '''
 
+from enum import Enum
+CommandType = Enum('A_COMMAND', 'C_COMMAND', 'L_COMMAND')
+
+import re
+
+
 
 class Parser:
+    
+    re_A_COMMAND = re.compile('@[A-Z]')
+    
+    def __init__(self, filename):
+        self.infile = open('workfile', 'r')        
+        self.moreCommands = False
+        self.currentCommand = None
 
-    def __init__(self, name):
-        self.name = name
-        self.tricks = []    # creates a new empty list for each dog
+    '''
+    Are there more commands in the input?
+    '''
+    def hasMoreCommands(self, trick):
+        return self.moreCommands
 
-    def add_trick(self, trick):
-        self.tricks.append(trick)
+    '''
+    Reads the next command from the input and makes it the current
+    command. Should be called only if hasMoreCommands is true.
+    Initially, there is no current command
+    '''
+    def advance(self)
+        return None
+
+    def commandType(self):
+        if 
